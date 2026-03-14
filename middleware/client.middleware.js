dcbc7e85f10371d079d88806ff5ec1c5895940b2
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2026-03-14 22:26:02 +0700

    update backed

--- middleware/client.middleware.js
+++ middleware/client.middleware.js
@@ -1,72 +1,38 @@
-// const jwt = require("jsonwebtoken");
-// const Client = require("../model/client.model");
-
-// const clientProtect = async (req, res, next) => {
-//   try {
-//     let token;
-
-//     if (req.cookies?.token) {
-//       token = req.cookies.token;
-//     } 
-//     else if (req.headers.authorization?.startsWith("Bearer ")) {
-//       token = req.headers.authorization.split(" ")[1];
-//     }
-
-//     if (!token) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Not authorized, token missing",
-//       });
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     const client = await Client.findById(decoded.id);
-
-//     if (!client) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "Client not found",
-//       });
-//     }
-
-//     req.client = client;
-
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "Invalid or expired token",
-//     });
-//   }
-// };
-
-// module.exports = { clientProtect };
-
-
 const jwt = require("jsonwebtoken");
 const Client = require("../model/client.model");
 
 const clientProtect = async (req, res, next) => {
-
   try {
 
     let token;
 
-    if (req.headers.authorization?.startsWith("Bearer")) {
+    if (req.headers.authorization?.startsWith("Bearer ")) {
       token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token && req.cookies?.token) {
+      token = req.cookies.token;
     }
 
     if (!token) {
       return res.status(401).json({
         success: false,
-        message: "Not authorized",
+        message: "Not authorized, token missing",
       });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.client = await Client.findById(decoded.id).select("-password");
+    const client = await Client.findById(decoded.id).select("-password");
+
+    if (!client) {
+      return res.status(401).json({
+        success: false,
+        message: "Client not found",
+      });
+    }
+
+    req.client = client;
 
     next();
 
@@ -74,11 +40,10 @@ const clientProtect = async (req, res, next) => {
 
     res.status(401).json({
       success: false,
-      message: "Token invalid",
+      message: "Token invalid or expired",
     });
 
   }
-
 };
 
-module.exports = {clientProtect};
+module.exports = { clientProtect };
