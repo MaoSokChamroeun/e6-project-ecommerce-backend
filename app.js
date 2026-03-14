73c318e634fed6cfeecec6a81d0c5a35d64bde0c
Mao Sokchamroeun <maosokchomroeun@gmail.com>
2026-03-14 18:14:35 +0700

    update env

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const cartRouter = require("./routes/cart.route");
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.USER_URL,
   credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
